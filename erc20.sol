6ba14eca361c247b989f88099388d3e90e50caa1
basicbnb <146373992+basicbnb@users.noreply.github.com>
2024-11-11 14:04:53 +0100

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -121,8 +121,7 @@ pragma solidity ^0.8.20;
  * sender of the transaction and its data. While these are generally available
  * via msg.sender and msg.data, they should not be accessed in such a direct
  * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
+ * paying for execution may not be the actual sender.
  *
  * This contract is only required for intermediate, library-like contracts.
  */
